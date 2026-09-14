7c32d1944dd0565b457eb96bf6f21bb34b6c998a
Julian Fortune <juliandfortune@gmail.com>
2026-09-13 17:27:50 -0700

    Upgrading to JVM 21

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,8 +32,6 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
 }
 
 kotlin {
-    jvmToolchain(20)
-
     jvm {
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
